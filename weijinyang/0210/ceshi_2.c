4922a011c93600ac78627add781b15e85e80cca3
weijinyang <wjy971208@163.com>
2020-02-10 19:21:39 +0800

    0210

---
+++ weijinyang/0210/ceshi_2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+
+void sys_err(const char* str,int errno)
+{
+//    strerror(errno);//fan yi  cuo wu hao
+    fprintf(stderr, "%s: %s", str, strerror(errno));
+    exit(1);
+}
+
+void * fun1()
+{
+    exit(0);
+    //return NULL;
+}
+void* pthread_fun(void * arg)//xian  cheng  zhu  han  shu  
+{//zi  xian  cheng
+   
+   
+    printf("pid = %d, tid = %lu\n", getpid(), pthread_self());
+    fun1();
+    printf("ssssssssssss\n");
+    return NULL;
+}
+int main(int argc, char* argv[])//zhu  xian  cheng
+{
+    pthread_t tid1;
+    pthread_create(&tid1, NULL, pthread_fun, NULL);
+
+
+    sleep(1);
+    printf("sssssssssssssss");
+    return 0;//exit(0);jieshujincheng  shifangkongjian
+}
+
